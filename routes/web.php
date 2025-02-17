abb58684dc5bade378d394ecb53421ab5b9e8017
Радионцев Денис <radioncev.denis@4px.ru>
2025-02-17 15:14:15 +0300

    Установить breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 
 Route::get('/', [DashboardController::class, 'index']);
-Route::get('/{widgetModel}', [DashboardController::class, 'detail'])->name('widgetDetail');
+Route::get('/widget/{widgetModel}', [DashboardController::class, 'detail'])->name('widgetDetail');
+
+Route::get('/admin', function () {
+    return view('admin');
+})->middleware(['auth', 'verified'])->name('admin');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
